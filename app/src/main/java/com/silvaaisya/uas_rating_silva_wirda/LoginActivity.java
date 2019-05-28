1dca417b01c32784a7fd3b3608343ce8b6649542
SilvaAisya <silvakdr68@gmail.com>
2019-05-29 05:52:10 +0700

    Login, register and layout

---
+++ app/src/main/java/com/silvaaisya/uas_rating_silva_wirda/LoginActivity.java
@@ -0,0 +1,75 @@
+package com.silvaaisya.uas_rating_silva_wirda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+    public class LoginActivity extends AppCompatActivity{
+        private EditText txt_email, txt_password;
+        private FirebaseAuth firebaseAuth;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
+
+            txt_email = findViewById(R.id.email);
+            txt_password = findViewById(R.id.password);
+            firebaseAuth = FirebaseAuth.getInstance();
+
+            if (firebaseAuth.getCurrentUser() != null) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            }
+
+        }
+
+        public void btn_reg(View view) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+
+        public void btnuserlog(View v) {
+            String email = txt_email.getText().toString();
+            final String password = txt_password.getText().toString();
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(), "Masukkan Email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getApplicationContext(), "Masukkan password!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            (firebaseAuth.signInWithEmailAndPassword(email, password)).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                        Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                        //i.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
+                        startActivity(i);
+                        finish();
+                    } else {
+                        Log.e("Error", task.getException().toString());
+                        Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                }
+            });
+
+        }
+    }
